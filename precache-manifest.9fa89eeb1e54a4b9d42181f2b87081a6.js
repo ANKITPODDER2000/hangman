df29cfdcb3c25d950420c8ed6e7aa81943f31a5a
Ankit Podder <ankitpodder0211@gmail.com>
2020-09-28 00:32:32 +0530

    Updates

---
+++ precache-manifest.9fa89eeb1e54a4b9d42181f2b87081a6.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17bd50b548f806ffa67e6959ee22b8a9",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "fe5f03633bb5196fce0a",
+    "url": "/hangman/static/css/main.9466c04f.chunk.css"
+  },
+  {
+    "revision": "bb93fe4a59d4f8617ed6",
+    "url": "/hangman/static/js/2.35335726.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman/static/js/2.35335726.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe5f03633bb5196fce0a",
+    "url": "/hangman/static/js/main.ec9f3320.chunk.js"
+  },
+  {
+    "revision": "90a5c66598860aa4f8b3",
+    "url": "/hangman/static/js/runtime-main.1889333a.js"
+  },
+  {
+    "revision": "f06df755b65f5f4d5c3b0a27a2d38f57",
+    "url": "/hangman/static/media/0.f06df755.jpg"
+  },
+  {
+    "revision": "ca6c10d5826783b2e21485e1e92df1ce",
+    "url": "/hangman/static/media/1.ca6c10d5.jpg"
+  },
+  {
+    "revision": "c80394c2a96ffad09a9df4e4815745f2",
+    "url": "/hangman/static/media/10.c80394c2.jpg"
+  },
+  {
+    "revision": "1dc765683556a2c05162544cff514491",
+    "url": "/hangman/static/media/2.1dc76568.jpg"
+  },
+  {
+    "revision": "837148ce5a3b2f485463adac61b033ec",
+    "url": "/hangman/static/media/3.837148ce.jpg"
+  },
+  {
+    "revision": "db9097e874e68b7ed7115ff05ffcb880",
+    "url": "/hangman/static/media/4.db9097e8.jpg"
+  },
+  {
+    "revision": "fbff4784004cc97463c883477a0090d1",
+    "url": "/hangman/static/media/5.fbff4784.jpg"
+  },
+  {
+    "revision": "c4ea9528b67823bd94c90de3d97d75a3",
+    "url": "/hangman/static/media/6.c4ea9528.jpg"
+  },
+  {
+    "revision": "62a781c89c590abb27129638d9d18c4d",
+    "url": "/hangman/static/media/7.62a781c8.jpg"
+  },
+  {
+    "revision": "0f8ca81ee9f7b2bc27102267f9e23864",
+    "url": "/hangman/static/media/8.0f8ca81e.jpg"
+  },
+  {
+    "revision": "1b557aa7ccfa88b6b3f4723c27d3f2d6",
+    "url": "/hangman/static/media/9.1b557aa7.jpg"
+  }
+]);
